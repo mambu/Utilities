e283e3fefb77cc47442a0400fda8512b5dc5c8c0
Marco Ambu <marco.ambu@gmail.com>
2011-04-15 09:13:15 -0700

    Added -v parameter to produce verbose output Fixed error in parsing options Using $() syntax to call external commands

--- teetime/teetime.sh
+++ teetime/teetime.sh
@@ -1,32 +1,45 @@
 #!/bin/bash
 
 usage() {
-  echo "The same as tee but prepend each line with the time the line is received."
+  echo "Teetime: The same as tee but prepend each line with the current time when the data is received."
   echo ""
   echo "Usage:"
-  echo `basename $0` "[-h] [-a] file"
+  echo $(basename $0) "[-h] [-a] [-v] filename"
   echo "Parameters:"
-  echo -e "\tfile\tname of file to write to"
+  echo -e "\tfilename\tFile to write to"
   echo "Options:"
-  echo -e "\t-a\tAppends to file"
+  echo -e "\t-a\tAppend to file"
+  echo -e "\t-v\tProduce verbose output"
   echo -e "\t-h\tShow usage and exit"
 }
 
-APPEND=
+printVerbose() {
+  if [ $VERBOSE ]
+  then
+    echo "$1"
+  fi
+}
 
-while getopts ":ah" opt; do
+APPEND=    # Specify if content file should be appended to output file
+VERBOSE=   # Specify if verbose oputput should be produced
+
+while getopts ":ahv" opt; do
     case $opt in
         a) APPEND=1
+            ;;
+        v) VERBOSE=1
+           printVerbose "Using Verbose output"
             ;;
         h) usage
             exit 0
             ;;
         *) usage
+            echo "Option not recognized: $OPTARG"
             exit 1
             ;;
     esac
-    shift
 done
+shift $(($OPTIND - 1))
 
 if [ ! $1 ]
 then
@@ -35,16 +48,21 @@ then
   exit 1
 fi
 
-echo "Redirecting to $1"
+printVerbose "Redirecting to $1"
 
-if [ ! $APPEND ] && [ -e $1 ]
+if [ ! $APPEND ]
 then
-  rm $1
+  if [ -e $1 ]
+  then
+    rm $1
+  fi
+else
+  printVerbose "Appending to file"
 fi
 
 while read myline
 do
-  Line="[`date +%T`] $myline"
+  Line="[$(date +%T)] $myline"
   echo $Line
   if [ $1 ]
   then
@@ -52,3 +70,4 @@ do
   fi
 done
 
+exit 0
